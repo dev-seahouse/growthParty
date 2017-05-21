1829ec41dc2c1acb18c7b03845195abe0db0e3eb
dev-seahouse <xinkenan.11@hotmail.con>
2017-05-22 05:21:57 +0800

    why join us layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,10 +21,44 @@
 @endsection
 
 @section('content')
-<div class="row">
+<div class="row lighter-primary-bg">
     <div class="small-10 medium-12 columns">
-        <h1 class="medium-text-center">Fund your <span class="pale-red-text">passion</span> by growing business.</h1>
+        <h2 class="medium-text-center">Fund your <span class="pale-red-text">passion</span> by growing business.</h2>
         <p class="medium-text-center"><strong class="dark-text">Help business owners.Risk-Free. Reap rewards.</strong></p>
     </div>
 </div>
+ 
+<div class="row">
+  <div class="small-12 columns">
+    <h1 class="teal-gray-text">Why you'll love joining this party</h1>
+  </div>
+</div>
+
+<!-- why join us -->
+<div class="row medium-text-center">
+
+  <div class="small-12 medium-4 columns">
+    <h3 class="pale-red-text">Extra income</h3>
+    <p>
+        With more companies on Growth Party, the  opportunities to earn are endless so is funding  your passion.
+    </p>
+  </div>
+
+  <div class="small-12 medium-4 columns">
+    <h3 class="pale-red-text">Step-by-step</h3>
+    <p>We are your partner. Tools and steps are  prescribed to bring you to success.</p>
+  </div>
+
+  <div class="small-12 medium-4 columns">
+    <h3 class="pale-red-text">
+      Flexibility
+    </h3>
+    <p>It’s your lifestyle, set your days  and decide when to earn and how often.</p>
+  </div>
+</div>
+<!-- ends why join us -->
+
+<!-- how it works -->
+<!-- end how it works -->
+
 @endsection
