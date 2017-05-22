15b4d43028ca975d4651d026378b894a276ccc91
dev-seahouse <xinkenan.11@hotmail.com>
2017-05-22 13:59:51 +0800

    add thin header class

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -2,64 +2,67 @@
 <html class="no-js" lang="{{ config('app.locale') }}">
 
 <head>
-    <meta charset="utf-8" />
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta property="og:title" content="">
-    <meta property="og:description" content="">
-    <meta property="og:image" content="{{ asset('img/logo.png') }}">
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+  <meta charset="utf-8" />
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta property="og:title" content="">
+  <meta property="og:description" content="">
+  <meta property="og:image" content="{{ asset('img/logo.png') }}">
+  <!-- CSRF Token -->
+  <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>
-        @section('title') 
-            {{ config('app.name', 'Growth Party') }}
-        @show
-    </title>
+  <title>
+    @section('title') {{ config('app.name', 'Growth Party') }} @show
+  </title>
 
-    <link rel="icon" href="/favicon.ico">
-    <link href="https://fonts.googleapis.com/css?family=Lato:400,700" rel="stylesheet">
-    <link href="/css/app.css" rel="stylesheet"> 
-    @stack('page_styles') <!-- page specific styles -->
-    
+  <link rel="icon" href="/favicon.ico">
+  <link href="https://fonts.googleapis.com/css?family=Lato:400,700,900" rel="stylesheet">
+  <link href="/css/app.css" rel="stylesheet"> @stack('page_styles')
+  <!-- page specific styles -->
 
-    <!--[if lt IE 9]>
+
+  <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
       <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
 
-    <!-- csrf script -->
-    <script>
-        window.Laravel = {!!json_encode(['csrfToken' => csrf_token()])!!};
-    </script>
+  <!-- csrf script -->
+  <script>
+    window.Laravel = {!!json_encode(['csrfToken' => csrf_token()])!!};
+  </script>
 
 </head>
 
 <body>
-       <div id="app">
-           
-       </div> 
-        <!-- navigation bar -->
-        @yield('nav')
-        <!-- end navigation bar --> 
+  <div id="app">
 
-        <!-- header content, e.g jumpbotron , slider -->
-        @yield('header')
-        <!-- end header -->
+  </div>
+  <!-- navigation bar -->
+  @yield('nav')
+  <!-- end navigation bar -->
 
-        <!-- main content -->
-        @yield('content')
-        <!-- end main content -->
+  <!-- header content, e.g jumpbotron , slider -->
+  @yield('header')
+  <!-- end header -->
 
-    
-    <script src="/js/manifest.js"></script><!-- webpack runtime -->
-    <script src="/js/vendor.js"></script><!-- vendor library -->
-    <script src="/js/app.js"></script><!-- application wide script -->
-    <script>
-        var _token = '{!! Session::token() !!}';
-        var _url = '{!! url("/") !!}';
-    </script>
-    @stack('page_scripts')<!-- page js libs/scripts -->   
+  <!-- main content -->
+  @yield('content')
+  <!-- end main content -->
+
+
+  <script src="/js/manifest.js"></script>
+  <!-- webpack runtime -->
+  <script src="/js/vendor.js"></script>
+  <!-- vendor library -->
+  <script src="/js/app.js"></script>
+  <!-- application wide script -->
+  <script>
+    var _token = '{!! Session::token() !!}';
+    var _url = '{!! url("/") !!}';
+
+  </script>
+  @stack('page_scripts')
+  <!-- page js libs/scripts -->
 
 
 </body>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,14 +1,11 @@
-@extends('layouts.master')
-
+@extends('layouts.master') 
 @push("page_styles")
-<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/motion-ui/1.2.2/motion-ui.css">
-@endpush
-
-@section('nav')
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/motion-ui/1.2.2/motion-ui.css"> 
+@endpush @section('nav')
 
 <div class="title-bar" data-responsive-toggle="top-menu" data-hide-for="medium">
   <button class="menu-icon" type="button" data-toggle></button>
-  <h2 class="title-bar-title">Growth Party</h2>
+  <h3 class="title-bar-title">Growth Party</h3>
   <button class="button">Start Earning</button>
 </div>
 
@@ -23,39 +20,37 @@
   </div>
 </div>
 
-@endsection
-
-@section('content')
+@endsection @section('content')
 <div class="row">
-    <div class="small-12 columns lighter-primary-bg">
-        <h1 class="medium-text-center">Fund your <span class="pale-red-text">passion</span> by growing business.</h1>
-        <p class="medium-text-center"><strong class="dark-text">Help business owners.Risk-Free. Reap rewards.</strong></p>
-    </div>
+  <div class="small-12 columns lighter-primary-bg">
+    <h1 class="medium-text-center">Fund your <span class="pale-red-text">passion</span> by growing business.</h1>
+    <p class="medium-text-center"><strong class="dark-text">Help business owners.Risk-Free. Reap rewards.</strong></p>
+  </div>
 </div>
 
 <!-- why join us -->
 <div class="row">
-    <div class="small-12 columns">
-        <h3>Why you'll love joining this party.</h3>  
-    </div>
+  <div class="small-12 columns">
+    <h3>Why you'll love joining this party.</h3>
+  </div>
 </div>
 
 <div class="row medium-text-center">
 
-    <div class="small-12 medium-4 columns">
-        <h3 class="pale-red-text">Extra income</h3>
-        <p>With more companies on Growth Party, the  opportunities to earn are endless so is funding  your passion.</p>
-    </div>
+  <div class="small-12 medium-4 columns">
+    <h3 class="pale-red-text">Extra income</h3>
+    <p>With more companies on Growth Party, the opportunities to earn are endless so is funding your passion.</p>
+  </div>
 
-    <div class="small-12 medium-4 columns">
-        <h3 class="pale-red-text">Step-by-step</h3>
-        <p>We are your partner. Tools and steps are  prescribed to bring you to success.</p> 
-    </div>
+  <div class="small-12 medium-4 columns">
+    <h3 class="pale-red-text">Step-by-step</h3>
+    <p>We are your partner. Tools and steps are prescribed to bring you to success.</p>
+  </div>
 
-    <div class="small-12 medium-4 columns">
-       <h3 class="pale-red-text">Flexibility</h3>
-       <p>It’s your lifestyle, set your days  and decide when to earn and how often.</p>
-    </div>
+  <div class="small-12 medium-4 columns">
+    <h3 class="pale-red-text">Flexibility</h3>
+    <p>It’s your lifestyle, set your days and decide when to earn and how often.</p>
+  </div>
 
 </div>
 
@@ -66,15 +61,7 @@
 
 <div class="row">
   <div class="columns">
-    <h2>Orbit</h2>
-    <p>Orbit doesn't automatically generate any HTML for you, giving you the flexibility to move around the various pieces of the plugin. Here's a complete example—we'll break down the individual pieces farther down.</p>
-  </div>
-</div>
-
-<div class="row">
-  <div class="columns">
-    <h2>Orbit - Text Slider</h2>
-    <p>A carousel slide can contain images or HTML—you can even mix between slides in one carousel!</p>
+    <h2>How it works</h2>
   </div>
 </div>
 
@@ -84,32 +71,34 @@
       <ul class="orbit-container">
         <button class="orbit-previous" aria-label="previous"><span class="show-for-sr">Previous Slide</span>&#9664;</button>
         <button class="orbit-next" aria-label="next"><span class="show-for-sr">Next Slide</span>&#9654;</button>
+        
         <li class="is-active orbit-slide">
           <div class="docs-example-orbit-slide">
-            <p><strong>This is dodgerblue.</strong> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <h4 class="pale-red-text">Step 1</h4>
+            <h2 class="pale-red-text thin-header">Choose</h2>
+            <p>Become a growth affiliate, choose the businesses you want to help and get paid for your efforts.</p>
           </div>
         </li>
+        
         <li class="orbit-slide">
           <div class="docs-example-orbit-slide">
-            <p><strong>This is rebeccapurple.</strong> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <h4 class="pale-red-text">Step 2</h4>
+            <h2 class="pale-red-text thin-header">Earn</h2>
+            <p>Use the Growth Party platform to promote and communicate from your desktop or on the go. This can be creative!</p>
           </div>
         </li>
+        
         <li class="orbit-slide">
           <div class="docs-example-orbit-slide">
             <p><strong>This is darkgoldenrod.</strong> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
           </div>
         </li>
-        <li class="orbit-slide">
-          <div class="docs-example-orbit-slide">
-            <p><strong>This is lightseagreen.</strong> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-          </div>
-        </li>
+        
       </ul>
       <nav class="orbit-bullets">
         <button class="is-active" data-slide="0"><span class="show-for-sr">First slide details.</span><span class="show-for-sr">Current Slide</span></button>
         <button data-slide="1"><span class="show-for-sr">Second slide details.</span></button>
         <button data-slide="2"><span class="show-for-sr">Third slide details.</span></button>
-        <button data-slide="3"><span class="show-for-sr">Fourth slide details.</span></button>
       </nav>
     </div>
   </div>
@@ -124,9 +113,8 @@
 
 <!-- Why they love the party -->
 <div class="row">
-    
+
 </div>
 <!-- end why they love the party -->
 
 @endsection
-
