b518dc59d0a0eb3254a7d32d1d8cbc8031da8003
dev-seahouse <xinkenan.11@hotmail.com>
2017-05-26 14:06:27 +0800

    extract lib

--- webpack.mix.js
+++ webpack.mix.js
@@ -21,4 +21,4 @@ mix.options({
 mix.sass('resources/assets/sass/app.scss', 'public/css')
    .js('resources/assets/js/foundation.js', 'public/js')
    .js('resources/assets/js/app.js', 'public/js')
-    .extract(['vue', 'axios', 'jquery'])
+    .extract(['vue', 'axios', 'jquery','jquery.counterup'])
