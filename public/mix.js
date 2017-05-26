2730a21da3c5bddca3d04188ad5c9d1b07518d3d
dev-seahouse <xinkenan.11@hotmail.com>
2017-05-26 22:09:00 +0800

    add reponsive classes

--- public/mix.js
+++ public/mix.js
@@ -1,1 +1,26 @@
-webpackJsonp([3],{14:function(n,o){},15:function(n,o){},70:function(n,o,c){c(15),n.exports=c(14)}},[70]);
+webpackJsonp([3],{
+
+/***/ 14:
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+
+/***/ 15:
+/***/ (function(module, exports) {
+
+
+
+/***/ }),
+
+/***/ 70:
+/***/ (function(module, exports, __webpack_require__) {
+
+__webpack_require__(15);
+module.exports = __webpack_require__(14);
+
+
+/***/ })
+
+},[70]);
