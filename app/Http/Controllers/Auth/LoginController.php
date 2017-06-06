1574a6592e5dedf48359b2669c1ce6dc1b1e1941
dev-seahouse <xinkenan.11@hotmail.com>
2017-06-06 20:25:18 +0800

    add authentication logic

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,37 +3,89 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use DeepCopy\Filter\Filter;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles authenticating users for the application and
-    | redirecting them to your home screen. The controller uses a trait
-    | to conveniently provide its functionality to your applications.
-    |
-    */
+  /*
+  |--------------------------------------------------------------------------
+  | Login Controller
+  |--------------------------------------------------------------------------
+  |
+  | This controller handles authenticating users for the application and
+  | redirecting them to your home screen. The controller uses a trait
+  | to conveniently provide its functionality to your applications.
+  |
+  */
 
-    use AuthenticatesUsers;
+  use AuthenticatesUsers;
 
-    /**
-     * Where to redirect users after login.
-     *
-     * @var string
-     */
-    protected $redirectTo = '/home';
+  /**
+   * Where to redirect users after login.
+   *
+   * @var string
+   */
+  protected $redirectTo = '/home';
 
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('guest')->except('logout');
+  /**
+   * What is the name attribute of the id field?
+   * @var string
+   */
+  protected $userIdFieldName = "loginId";
+
+  /**
+   * User is using email or mobile number to login?
+   * @var string
+   */
+  protected $loginIdType="email";
+
+  public function login(Request $request)
+  {
+    $this->setIdType($request);
+
+    $this->validateLogin($request);
+
+    if ($this->hasTooManyLoginAttempts($request)) {
+      $this->fireLockoutEvent($request);
+
+      return $this->sendLockoutResponse($request);
     }
+
+    if ($this->attemptLogin($request)) {
+      return $this->sendLoginResponse($request);
+    }
+
+    $this->incrementLoginAttempts($request);
+
+    return $this->sendFailedLoginResponse($request);
+
+  }
+
+  public function username()
+  {
+    return $this->loginIdType;
+  }
+
+  protected function setIdType(Request $request)
+  {
+    if (is_numeric($request->input($this->userIdFieldName))){
+      $this->loginIdType = "mobile";
+    }else if (filter_var($request->input($this->userIdFieldName),FILTER_VALIDATE_EMAIL)){
+      $this->loginIdType = "email";
+    }
+  }
+
+  /**
+   * Create a new controller instance.
+   *
+   * @return void
+   */
+  public function __construct()
+  {
+    $this->middleware('guest')->except('logout');
+  }
+
+
 }
