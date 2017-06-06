06aac4e62a3ae15ec289347f796bfd93ccf29884
dev-seahouse <xinkenan.11@hotmail.com>
2017-06-06 20:23:32 +0800

    reindent file

--- resources/assets/js/components/Navigation.vue
+++ resources/assets/js/components/Navigation.vue
@@ -1,60 +1,59 @@
 <template>
-<nav>
-  <div class="title-bar drop-shadow" data-responsive-toggle="top-menu"  data-hide-for="medium" >
-    <button class="menu-icon" type="button" data-toggle="top-menu"></button>
-    <img src="images/logo.jpe" alt="" class="logo"/>
-    <h4 class="title-bar-title l-logo-text show-for-medium">Growth Party</h4>
-    <div data-magellan>
-      <a class="register button m-b-0" href="#registerForm">Join Our Beta</a>
+  <nav>
+    <div class="title-bar drop-shadow" data-responsive-toggle="top-menu" data-hide-for="medium">
+      <button class="menu-icon" type="button" data-toggle="top-menu"></button>
+      <img src="images/logo.jpe" alt="" class="logo"/>
+      <h4 class="title-bar-title l-logo-text show-for-medium">Growth Party</h4>
+      <div data-magellan>
+        <a class="register button m-b-0" href="#registerForm">Join Our Beta</a>
+      </div>
     </div>
-  </div>
 
-  <div class="top-bar drop-shadow" id="top-menu">
+    <div class="top-bar drop-shadow" id="top-menu">
 
-    <div class="top-bar-left">
-       <ul class="vertical medium-horizontal menu align-right" data-dropdown-menu>
-          <li class="menu-text show-for-medium flex-align-left"><img src="images/logo.jpe" alt="company logo" class="logo">Growth Party</li>
+      <div class="top-bar-left">
+        <ul class="vertical medium-horizontal menu align-right" data-dropdown-menu>
+          <li class="menu-text show-for-medium flex-align-left"><img src="images/logo.jpe" alt="company logo"
+                                                                     class="logo">Growth Party
+          </li>
           <li class="menu-text"><a data-open="loginModal">Login</a></li>
-      </ul>
-    </div>
-
-    <div data-magellan>
-      <a class="register button show-for-medium m-b-0" href="#registerForm">Join Our Beta</a>
-    </div>
-
-  </div>
-
-<!-- <div class="small reveal" id="loginModal" data-reveal> -->
-<modal class="small" id="loginModal" closable=true>
-  <h3 slot="title">Login</h3>
-  <form action="/login" method="post" accept-charset="utf-8">
-    <input type="hidden" name="_token" :value="csrf_token">
-    <div class="row">
-      <div class="small-12 columns">
-        <label>Email or phone<input type="text" name="loginId" placeholder="Enter email or mobile"></label>
+        </ul>
       </div>
-    </div>
-    <div class="row">
-      <div class="small-12 columns">
-        <label>Password<input type="password" name="password" placeholder="Enter password"></label>
+
+      <div data-magellan>
+        <a class="register button show-for-medium m-b-0" href="#registerForm">Join Our Beta</a>
       </div>
-    </div>
-    <div class="row column">
-        <button type="submit" class="button">Login</button>
-    </div>
-  </form>
 
-</modal>
-<!-- </div> -->
+    </div>
 
-</nav>
+    <modal class="small" id="loginModal" closable=true>
+
+      <h3 slot="title">Login</h3>
+      <form action="/login" method="post" accept-charset="utf-8">
+        <input type="hidden" name="_token" :value="csrf_token">
+        <div class="row">
+          <div class="small-12 columns">
+            <label>Email or phone<input type="text" name="loginId" placeholder="Enter email or mobile"></label>
+          </div>
+        </div>
+        <div class="row">
+          <div class="small-12 columns">
+            <label>Password<input type="password" name="password" placeholder="Enter password"></label>
+          </div>
+        </div>
+        <div class="row column">
+          <button type="submit" class="button">Login</button>
+        </div>
+      </form>
+    </modal>
+
+  </nav>
 
 </template>
 
 <script>
-    export default {
-        methods: {
-        }
+  export default {
+    methods: {}
 
-    }
+  }
 </script>
