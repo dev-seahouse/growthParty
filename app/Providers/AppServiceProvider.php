05323c607ae03d15fb29a53b64ff840e7a9fe097
dev-seahouse <xinkenan.11@hotmail.con>
2017-06-08 11:35:47 +0800

    add ide helper

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
 
@@ -25,5 +26,8 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         //
+      if ($this->app->environment() !== 'production') {
+        $this->app->register(IdeHelperServiceProvider::class);
+      }
     }
 }
