0a1111d24b75a5e32fadeeb97edf0e2d7b66e8dd
dev-seahouse <xinkenan.11@hotmail.com>
2017-06-09 11:10:56 +0800

    posts
    add styles for poasts

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -14,7 +14,16 @@
     <div class="l-padded row">
       @foreach($chunk as $post)
         <div class="small-12 medium-6 columns">
-        
+          <div class="row">
+            <div class="medium-12 large-6 columns">
+              <img src="/storage/{{$post->image}}" alt="">
+            </div>
+            <div class="small-12 large-6 columns">
+              <h4>{{$post->title}}</h4>
+              <p>{{$post->excerpt}}</p>
+            </div>
+          </div>
+
         </div>
       @endforeach
     </div>
