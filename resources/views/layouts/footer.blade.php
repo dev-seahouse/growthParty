0af2d6e4e8945acaa540ad06ea2b4b2c67dc9307
Kenan <xinkenan.11@hotmail.com>
2017-06-10 16:24:30 +0800

    duno waht

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,10 +1,11 @@
 <footer class="l-footer">
-  <div class="text-center secondary-text">
-    @2017 Growth Party. All rights reserved.
-  </div>
+
   <div class="footer-bar">
     <li><a href="#" title="Terms Of Use">Terms of Use</a></li>
     <li><a href="#" title="Privacy Policy">Privacy Policy</a></li>
     <li><a href="#" title="Contact">Contact</a></li>
+  </div>
+    <div class="text-center secondary-text">
+    @2017 Growth Party. All rights reserved.
   </div>
 </footer>
