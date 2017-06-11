7eaf6b5140a6190f9e484de4df026f26d45069cd
dev-seahouse <xinkenan.11@hotmail.com>
2017-06-12 00:01:35 +0800

    add stuff

--- after.sh
+++ after.sh
@@ -4,7 +4,8 @@
 # add any commands you wish to this file and they will
 # be run after the Homestead machine is provisioned.
 cd /home/vagrant/Projects/growthparty
+cp .env.vagrant .env
+php artisan key:generate
 composer install
 npm install
 npm run watch
-
