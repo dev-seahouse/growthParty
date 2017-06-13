d2f813949e3b2dcadedc3688da756829a6ba4a1a
dev-seahouse <xinkenan.11@hotmail.com>
2017-06-14 04:09:43 +0800

    add time ago and estimate reads

--- app/Post.php
+++ app/Post.php
@@ -18,8 +18,17 @@ class Post extends Model
     // retrieve user object
     return $this->authorId();
   }
-  public function getEstimatedRead()
+  public function getEstimatedReadAttribute()
   {
     $postContent = $this->body;
+    $word = str_word_count(strip_tags($postContent));
+    $m = floor($word / 190);
+    $s = floor($word % 190 / (190 / 60));
+    if ($m == 0){
+      $est = $s . ' second'. ($s==1 ? '' : 's');
+    }else {
+      $est = $m . ' minute'.($m == 1 ? '':'s');
+    }
+    return $est;
   }
 }
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -16,18 +16,18 @@
         <div class="small-12 medium-6 columns">
           <div class="card" data-equalizer-watch>
             
-            <div class="card-image">
+            <div class="card-image-container">
               <img src="/storage/{{$post->image}}" alt="">
             </div>
             
             <div class="card-content">
               <h4 class="title">{{$post->title}}</h4>
-              <p class="content">{{str_limit($post->excerpt, 150)}}</p>
+              <p class="content">{{str_limit($post->excerpt, 100)}}</p>
               <a href="/blog/{{$post->slug}}">Read more ...</a>
               <div class="footer">
                 <img src="/storage/{{$post->author->avatar}}" class="profile-img" alt="">
                 <a href="">{{$post->author->name}}</a>
-                <span class="flex-pull-right">updated {{$post->updated_at->diffForHumans()}}</span>
+                <p class="info">{{$post->updated_at->diffForHumans()}} {{$post->estimated_read}} read </p>
               </div>
             </div>
           </div>
