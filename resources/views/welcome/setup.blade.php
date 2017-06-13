9ebbd0cb59f957815f4fded372e40444df815ed6
dev-seahouse <xinkenan.11@hotmail.com>
2017-06-13 11:35:47 +0800

    comments for upload logic

--- resources/views/welcome/setup.blade.php
+++ resources/views/welcome/setup.blade.php
@@ -24,9 +24,13 @@
 @push('page_scripts')
     <script src="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.0.1/min/dropzone.min.js"></script>
     <script>
+      // 1.upload image to local storage https://stackoverflow.com/questions/19183180/how-to-save-an-image-to-localstorage-and-display-it-on-the-next-page
+      // 2.on upload success display image in canvas
+      // 3.use croppie to crop
+      // 4.on form submit update image
       let dropzone = new Dropzone("#formUploadProfile", {
       });
-      
+
 
     </script>
 @endpush('page_scripts')
