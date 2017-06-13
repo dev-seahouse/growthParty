eb105edfe7fba1342a83d7e9f962c32e72621cea
dev-seahouse <xinkenan.11@hotmail.con>
2017-06-13 20:45:11 +0800

    profile pic and days ago

--- app/Post.php
+++ app/Post.php
@@ -3,11 +3,23 @@
 namespace App;
 
 use TCG\Voyager\Models\Post as Model;
+use TCG\Voyager\Models\User as User;
 
 class Post extends Model
 {
+  
   public function getRouteKeyName()
   {
     return "slug";
+  }
+  
+  public function author()
+  {
+    // retrieve user object
+    return $this->authorId();
+  }
+  public function getEstimatedRead()
+  {
+    $postContent = $this->body;
   }
 }
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -14,20 +14,27 @@
     <div class="l-padded row" data-equalizer data-equalize-on="medium">
       @foreach($chunk as $post)
         <div class="small-12 medium-6 columns">
-            <div class="card" data-equalizer-watch>
-            <div class="card-image" >
+          <div class="card" data-equalizer-watch>
+            
+            <div class="card-image">
               <img src="/storage/{{$post->image}}" alt="">
             </div>
+            
             <div class="card-content">
               <h4 class="title">{{$post->title}}</h4>
-              <p class="content">{{str_limit($post->excerpt, 250)}}</p>
+              <p class="content">{{str_limit($post->excerpt, 150)}}</p>
               <a href="/blog/{{$post->slug}}">Read more ...</a>
+              <div class="footer">
+                <img src="/storage/{{$post->author->avatar}}" class="profile-img" alt="">
+                <a href="">{{$post->author->name}}</a>
+                <span class="flex-pull-right">updated {{$post->updated_at->diffForHumans()}}</span>
+              </div>
             </div>
           </div>
-
+        
         </div>
       @endforeach
     </div>
-
+  
   @endforeach
 @endsection
