68b0f766cf9e4c9e9f6057a388275fb59bd5e03c
Marcus <engzhaowei.11@gmail.com>
2017-06-16 04:47:37 +0800

    Shift all the tracking scripts to master.blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -178,94 +178,7 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/typed.js/1.1.7/typed.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.2.1/owl.carousel.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.0/jquery.waypoints.min.js"></script>
-<!-- Hotjar Tracking Code for growthparty.co -->
-<script>
-    (function(h,o,t,j,a,r){
-        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-        h._hjSettings={hjid:533964,hjsv:5};
-        a=o.getElementsByTagName('head')[0];
-        r=o.createElement('script');r.async=1;
-        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-        a.appendChild(r);
-    })(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=');
-</script>
-<!-- Google analytics Tracking Code for growthparty.co -->
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-32995284-5', 'auto');
-  ga('send', 'pageview');
-
-</script>
-
-  <!— Start of HubSpot Embed Code —>
-  <script>
-    // HubSpot Script Loader. Please do not block this resource. See more: http://hubs.ly/H0702_H0
-    function loadForm(){
-        (function (id, src, attrs) {
-        if (document.getElementById(id)) { return; }
-        var js = document.createElement('script');
-        js.src = src;
-        js.type = 'text/javascript';
-        js.id = id;
-        for (var name in attrs) { if(attrs.hasOwnProperty(name)) { js.setAttribute(name, attrs[name]); } }
-        var e = document.getElementsByTagName('script')[0];
-        e.parentNode.insertBefore(js, e);
-      })('CollectedForms-3327713', 'https://js.hscollectedforms.net/collectedforms.js', {"crossorigin":"anonymous","data-leadin-portal-id":3327713,"data-leadin-env":"prod","data-loader":"hs-scriptloader"});
-
-      (function (id, src, attrs) {
-        if (document.getElementById(id)) { return; }
-        var js = document.createElement('script');
-        js.src = src;
-        js.type = 'text/javascript';
-        js.id = id;
-        for (var name in attrs) { if(attrs.hasOwnProperty(name)) { js.setAttribute(name, attrs[name]); } }
-        var e = document.getElementsByTagName('script')[0];
-        e.parentNode.insertBefore(js, e);
-      })('messages-3327713', 'https://api.usemessages.com/messages/v2/embed/3327713.js', {"data-loader":"hs-scriptloader"});
-
-      (function (id, src, attrs) {
-        if (document.getElementById(id)) { return; }
-        var js = document.createElement('script');
-        js.src = src;
-        js.type = 'text/javascript';
-        js.id = id;
-        for (var name in attrs) { if(attrs.hasOwnProperty(name)) { js.setAttribute(name, attrs[name]); } }
-        var e = document.getElementsByTagName('script')[0];
-        e.parentNode.insertBefore(js, e);
-      })('LeadFlows-3327713', 'https://js.hsleadflows.net/leadflows.js', {"crossorigin":"anonymous","data-leadin-portal-id":3327713,"data-leadin-env":"prod","data-loader":"hs-scriptloader"});
-
-      (function (id, src, attrs) {
-        if (document.getElementById(id)) { return; }
-        var js = document.createElement('script');
-        js.src = src;
-        js.type = 'text/javascript';
-        js.id = id;
-        for (var name in attrs) { if(attrs.hasOwnProperty(name)) { js.setAttribute(name, attrs[name]); } }
-        var e = document.getElementsByTagName('script')[0];
-        e.parentNode.insertBefore(js, e);
-      })('hs-analytics', '//js.hs-analytics.net/analytics/1496247900000/3327713.js', {"data-loader":"hs-scriptloader"});
-    }
-
-    loadForm();
-
-    function delete_cookie( name ) {
-      document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
-    }
-
-    window.onbeforeunload = function () {
-      delete_cookie('__hstc');
-      delete_cookie('__hssc');
-      delete_cookie('hubspotutk');
-      delete_cookie('__hssrc');
-      localStorage.clear();
-    };
-
-  </script>
-  <!— End of HubSpot Embed Code —>
   <script>
     $(function(){
       // init typed js
@@ -326,22 +239,3 @@
   </script>
 
 @endpush
-
-<script>
-  window.fbAsyncInit = function() {
-    FB.init({
-      appId      : 'your-app-id',
-      xfbml      : true,
-      version    : 'v2.8'
-    });
-    FB.AppEvents.logPageView();
-  };
-
-  (function(d, s, id){
-     var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = "//connect.facebook.net/en_US/sdk.js";
-     fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
-</script>
