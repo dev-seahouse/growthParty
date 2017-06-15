e8905e8f4fec33caf05513f1d457c88c4b8309c9
Unknown <engzhaowei.11@gmail.com>
2017-06-15 17:40:30 +0800

    update the hourglass

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -23,7 +23,7 @@
      <div class="footer">
          <p class="info">Posted by:
         <img style="display:none;" src="/storage/{{$post->author->avatar}}" width="30px" height="auto" class="profile-img" alt="{{$post->author->name}}">
-        <a href="">{{$post->author->name}}</a>&nbsp; <span class="info"> {{$post->created_at->diffForHumans()}}</span> &nbsp;&nbsp;:hourglass_flowing_sand:{{$post->estimated_read}} read </p>
+        <a href="">{{$post->author->name}}</a>&nbsp; <span class="info"> {{$post->created_at->diffForHumans()}}</span> &nbsp;&nbsp; ⏳{{$post->estimated_read}} read </p>
       </div>
       <img src="/storage/{{$post->image}}" alt="{!! $post->title !!}">
       <br/>
