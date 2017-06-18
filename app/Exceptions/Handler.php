f797cc33a8007895138b8dcc65c5972985375613
dev-seahouse <xinkenan.11@hotmail.con>
2017-06-18 13:49:13 +0800

    add response cache

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -66,7 +66,7 @@ class Handler extends ExceptionHandler
         if ($request->expectsJson()) {
             return response()->json(['error' => 'Unauthenticated.'], 401);
         }
-
+        
         return redirect()->guest(route('welcome'));
     }
 }
--- config/app.php
+++ config/app.php
@@ -168,6 +168,7 @@ return [
          */
         Laravel\Tinker\TinkerServiceProvider::class,
         TCG\Voyager\VoyagerServiceProvider::class,
+        Spatie\ResponseCache\ResponseCacheServiceProvider::class,
         /*
          * Application Service Providers...
          */
@@ -232,7 +233,7 @@ return [
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
         'Image' => Intervention\Image\Facades\Image::class,
-
+        'ResponseCache' => Spatie\ResponseCache\ResponseCacheFacade::class,
     ],
 
 ];
