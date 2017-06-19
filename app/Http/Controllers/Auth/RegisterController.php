72872c387ab675341c16e07ffd2601ec9a87411c
dev-seahouse <xinkenan.11@hotmail.com>
2017-06-19 10:43:10 +0800

    remove numberic rule from mobile num as it cause validation error

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -52,7 +52,7 @@ class RegisterController extends Controller
         return Validator::make($data, [
             //'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
-            'mobile' =>'required|numeric|digits:8',
+            'mobile' =>'required|digits:8',
             'password' => 'required|string|min:6',
         ]);
     }
