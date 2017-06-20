821c07a3d78ba833778e1daf6393103025bf0832
dev-seahouse <xinkenan.11@hotmail.con>
2017-06-20 17:54:36 +0800

    update navigation

--- resources/assets/js/components/Navigation.vue
+++ resources/assets/js/components/Navigation.vue
@@ -1,41 +1,51 @@
 <template>
-<nav>
-  <div class="title-bar drop-shadow" data-responsive-toggle="top-menu"  data-hide-for="medium" >
-    <button class="menu-icon" type="button" data-toggle="top-menu"></button>
-    <a class="flex-pull-left" href="/"><img src="/images/logo.jpe" alt="" class="l-logo-img"/></a>
-    <h4 class="title-bar-title l-logo-text show-for-medium">Growth Party</h4>
-    <div data-magellan>
-      <a class="register button m-b-0" href="/blog">Visit Blog</a>
+  <nav class="navbar fixed">
+    <div class="title-bar drop-shadow" data-responsive-toggle="top-menu" data-hide-for="medium">
+      <button class="menu-icon" type="button" data-toggle="top-menu"></button>
+      <a class="flex-pull-left" href="/"><img src="/images/logo.jpe" alt="" class="l-logo-img"/></a>
+      <h4 class="title-bar-title l-logo-text show-for-medium">Growth Party</h4>
+      <div data-magellan>
+        <a class="register button m-b-0" href="/blog">Visit Blog</a>
+      </div>
     </div>
-  </div>
-
+    
     <div class="top-bar drop-shadow p-v-0" id="top-menu">
-
+      
       <div class="top-bar-left">
         <ul class="vertical medium-horizontal menu align-right" data-dropdown-menu>
           <li class="menu-text show-for-medium flex-pull-left">
             <a href="/" class="logo"><img src="/images/logo.jpe" alt="company logo"
                                           class="l-logo-img"><span class="logo-text">Growth Party</span>
             </a>
-         </li>
-       </ul>
+          </li>
+        </ul>
       </div>
-
+      
       <div data-magellan>
         <a class="register button show-for-medium m-b-0" data-open="registerModal" href="/blog">Visit Blog</a>
       </div>
-
-  </div>
-</nav>
+    
+    </div>
+  </nav>
 </template>
 
 <script>
-    export default {
-        mounted() {
-            console.log('Component mounted.')
-        },
-        methods: {
+  export default {
+    mounted() {
+      var header = document.querySelector("nav.navbar");
+      var headroom = new Headroom(header,{
+          "offset": 205,
+          "tolerance": 5,
+          "classes": {
+            "initial": "animated",
+            "pinned": "slideDown",
+            "unpinned": "slideUp"
+          }
         }
-
-    }
+      );
+      headroom.init()
+    },
+    methods: {}
+    
+  }
 </script>
