032ae42ace1e0d9b493fc6ec6dbbdac9ba025d6d
Marcus <engzhaowei.11@gmail.com>
2017-06-20 11:13:15 +0800

    add facebook comment plugin on master

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -73,6 +73,15 @@
       ga('send', 'pageview');
 
     </script>
+    <!-Strat of FB Comment code->
+    <div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.9&appId=756709494453863";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
 
       <!— Start of HubSpot Embed Code —>
       <script>
