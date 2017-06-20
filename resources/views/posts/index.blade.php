f5e2fc6c93feb0063f032bc558f96bc1e8ac4afa
dev-seahouse <xinkenan.11@hotmail.com>
2017-06-21 04:03:45 +0800

    good night world

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -9,37 +9,17 @@
 @endsection
 
 @section('content')
-  <div class="fix-nav-cover-content">
+  <div class="main-container fix-nav-cover-content">
     {{--for each post in Posts, output post--}}
     @foreach($posts->chunk(2) as $chunk)
-      <div class="l-padded row" data-equalizer data-equalize-on="medium">
+      <div class="l-padded row" data-equalizer>
         @foreach($chunk as $post)
-          <div class="small-12 medium-6 columns">
-            <a href="/blog/{{$post->slug}}">
-              <div class="card" data-equalizer-watch>
-                <div class="card-image-container">
-                  <img src="/storage/{{$post->image}}" alt="">
-                </div>
-
-                <div class="card-content">
-                  <h4 style="color:black;" class="title">{{str_limit($post->title,90)}}</h4>
-                  <p style="color:rgba(0,0,0,.6)!important;" class="content">{{str_limit($post->excerpt, 140)}}</p>
-                  Read more ...
-                  <div class="footer">
-                    <img style="display:none;" src="/storage/{{$post->author->avatar}}" class="profile-img"
-                         alt="{{$post->author->name}}">
-                    <a style="display:none;" href="">{{$post->author->name}}</a>
-                    <p class="info">Posted {{$post->created_at->diffForHumans()}} &nbsp;&nbsp;
-                      <i class="fa fa-hourglass-half secondary-text" ></i> {{$post->estimated_read}}
-                      read </p>
-                  </div>
-                </div>
-            </a>
-          </div>
-
+            @include('posts.list_entry')
+        @endforeach
       </div>
-    @endforeach
+      <!-- end or a row -->
   </div>
+  <!-- end of main container -->
 
   @endforeach
   <div class="l-medium-h-padded row columns">
