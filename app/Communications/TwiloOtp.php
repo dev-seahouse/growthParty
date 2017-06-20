7d71516fae012254adf86b2e7359cda93bdba747
dev-seahouse <xinkenan.11@hotmail.com>
2017-06-20 14:06:14 +0800

    add twilo otp implementation

---
+++ app/Communications/TwiloOtp.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Communications;
+
+
+use DebugBar\DebugBar;
+use Session;
+use Twilio\Rest\Client;
+
+class TwiloOtp implements Otp
+{
+  private $message = "Welcome to the growthparty family!";
+  private $client;
+
+  public function __construct(Client $client)
+  {
+    $this->client=$client;
+  }
+
+  public function send($mobile)
+  {
+    $otp_num = mt_rand(0, 9999);
+    Session::put('twillo_token', $otp_num);
+    $sms = $this->client->messages->create(
+      $mobile,
+      array(
+        'from' => env('TWILO_NUMBER'),
+        'body' => $this->message . " Your one-time pin is " . $otp_num
+      )
+    );
+  }
+
+  public function validate($otp)
+  {
+    $validToken = Session::get('twillo_token');
+    if ($otp == $validToken) {
+      Session::forget('twillo_token');
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
