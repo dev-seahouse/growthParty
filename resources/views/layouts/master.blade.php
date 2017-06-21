1c0ff00699ff41e4e7532c31c05dbc1371521db3
dev-seahouse <xinkenan.11@hotmail.con>
2017-06-22 01:28:07 +0800

    remove typo

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -17,7 +17,6 @@
   <link rel="apple-touch-icon-precomposed" sizes="152x152" href="apple-touch-icon-152x152.png"/>
   <link rel="icon" type="image/png" href="favicon-196x196.png" sizes="196x196"/>
   <link rel="icon" type="image/png" href="favicon-96x96.png" sizes="96x96"/>
-  we
   <link rel="icon" type="image/png" href="favicon-32x32.png" sizes="32x32"/>
   <link rel="icon" type="image/png" href="favicon-16x16.png" sizes="16x16"/>
   <link rel="icon" type="image/png" href="favicon-128.png" sizes="128x128"/>
