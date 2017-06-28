9dd4447d7fa194e48c1037c74d39403e2217e9be
dev-seahouse <xinkenan.11@hotmail.com>
2017-06-29 04:56:22 +0800

    alter user table to use industry id instead of  industry

---
+++ database/migrations/2017_06_29_021402_alter_occupations_table_add_column_indstry_id.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterOccupationsTableAddColumnIndstryId extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+      Schema::table('occupations', function(Blueprint $table){
+        $table->unsignedInteger('industry_id');
+      });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+      Schema::table('occupations', function (Blueprint $table) {
+        $table->dropColumn('industry_id');
+      });
+    }
+}
