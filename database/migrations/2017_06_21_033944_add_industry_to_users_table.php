9b7fe9b248ef9112888888735a5a507d64b43c13
dev-seahouse <xinkenan.11@hotmail.com>
2017-06-29 04:57:13 +0800

    remove redundent modifier

--- database/migrations/2017_06_21_033944_add_industry_to_users_table.php
+++ database/migrations/2017_06_21_033944_add_industry_to_users_table.php
@@ -14,7 +14,7 @@ class AddIndustryToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('industry')->nullable()->default(null);
+            $table->string('industry')->nullable();
         });
     }
 
