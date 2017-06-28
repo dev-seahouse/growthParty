6503cbcc3438c9c6c1873c6025b618683aa327f5
dev-seahouse <xinkenan.11@hotmail.com>
2017-06-28 14:01:08 +0800

    add test user to usertableseeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -6,35 +6,43 @@ use TCG\Voyager\Models\User;
 
 class UsersTableSeeder extends Seeder
 {
-    /**
-     * Auto generated seed file.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        if (User::count() == 0) {
-            $adminRole = Role::where('name', 'admin')->firstOrFail();
-            $userRole = Role::where('name', 'user')->firstOrFail();
+  /**
+   * Auto generated seed file.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    if (User::count() == 0) {
+      $adminRole = Role::where('name', 'admin')->firstOrFail();
+      $userRole = Role::where('name', 'user')->firstOrFail();
 
-            User::create([
-                'name'           => 'Admin',
-                'email'          => 'marcus@growthparty.co',
-                'mobile'         => "90668301",
-                'password'       => bcrypt('password'),
-                'remember_token' => str_random(60),
-                'role_id'        => $adminRole->id,
-            ]);
+      User::create([
+        'name'           => 'Admin',
+        'email'          => 'marcus@growthparty.co',
+        'mobile'         => "90668301",
+        'password'       => bcrypt('password'),
+        'remember_token' => str_random(60),
+        'role_id'        => $adminRole->id,
+      ]);
 
-            DB::table('users')->insert([
-                'email' => "test@mail.com",
-                'password' => bcrypt('password'),
-                'mobile' => 87878787,
-                'remember_token' => str_random(60),
-                'role_id' => $userRole->id
-            ]);
+      DB::table('users')->insert([
+        'email'          => "test@mail.com",
+        'password'       => bcrypt('password'),
+        'mobile'         => 87878787,
+        'remember_token' => str_random(60),
+        'role_id'        => $userRole->id
+      ]);
+
+      DB::table('users')->insert([
+        'email'          => "test2@mail.com",
+        'password'       => bcrypt('password2'),
+        'mobile'         => 98231232,
+        'remember_token' => str_random(60),
+        'role_id'        => $userRole->id
+      ]);
 
 
-        }
     }
+  }
 }
