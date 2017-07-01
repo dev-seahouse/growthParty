2ab65ff75ac9cf932c86feb9a37cd679bc6f3fb7
dev-seahouse <xinkenan.11@hotmail.com>
2017-07-02 03:56:52 +0800

    create auto affliate  count update trigger

---
+++ database/migrations/2017_07_02_031436_create_affliate_count_trigger.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAffliateCountTrigger extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+      DB::unprepared('
+       CREATE TRIGGER tr_update_affiliate_count
+       AFTER INSERT
+       ON `program_user`
+       FOR EACH ROW 
+          UPDATE `programs`
+          SET `affiliate_count` = `affiliate_count` + 1
+          WHERE `id` = NEW.program_id
+      ');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+      DB::unprepared('DROP TRIGGER `tr_update_affiliate_count`');
+    }
+}
