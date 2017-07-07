74940a4992d442dff229445fedca51f366e0b3be
dev-seahouse <xinkenan.11@hotmail.com>
2017-07-08 03:12:23 +0800

    register programMatcher serviceProvider

---
+++ app/Programs/ProgramMatcherServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Programs;
+
+use Illuminate\Support\ServiceProvider;
+
+class ProgramMatcherServiceProvider extends ServiceProvider
+{
+  /**
+   * Bootstrap the application services.
+   *
+   * @return void
+   */
+  public function boot()
+  {
+  }
+
+  /**
+   * Register the application services.
+   *
+   * @return void
+   */
+  public function register()
+  {
+    $this->app->singleton(
+      ProgramMatcherImpl::class, function ($app) {
+        return new ProgramMatcherImpl();
+    });
+
+    $this->app->bind (
+      ProgramMatcher::class,
+      ProgramMatcherImpl::class
+    );
+  }
+}
