3c282cf6929cf6822330be9e0645b86a956653f2
dev-seahouse <xinkenan.11@hotmail.com>
2017-07-15 05:35:09 +0800

    modify program assigner to use pq

--- app/Programs/ProgramAssigner.php
+++ app/Programs/ProgramAssigner.php
@@ -15,15 +15,14 @@ class ProgramAssigner
 
   public function assign(User $user)
   {
-    // if user have less than user.maxNoProgramsAllowed, then assign the left over
-    // todo: there should be a maxMatches attribute
-    $numProgramsShouldHave = 3;
-    $userPrograms = $user->programs;
-    $numProgramsUserHave = $userPrograms->count();
+    $numProgramsShouldHave = $user->maxMatches;
+    $currentUserPrograms = $user->programs;
+    $numProgramsUserHave = $currentUserPrograms->count();
     $numProgramsToMatch = $numProgramsShouldHave - $numProgramsUserHave;
-    $matchedPrograms = $this->matcher->getMatches($user, $numProgramsToMatch);
-    if ($numProgramsToMatch !== 0 && !$userPrograms->contains($matchedPrograms)) {
-      $user->programs()->saveMany($matchedPrograms);
+    $matchingPrograms = $this->matcher->getMatches($user, $numProgramsToMatch);
+
+    if ($numProgramsToMatch !== 0 && !$currentUserPrograms->contains($matchingPrograms)) {
+      $user->programs()->saveMany($matchingPrograms);
     }
   }
 }
