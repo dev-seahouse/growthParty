d6a9e52f00c8863183a9581dcde30aaf3b665166
dev-seahouse <xinkenan.11@hotmail.com>
2017-07-15 02:59:27 +0800

    modify program table seeder to allow resuable program template

--- database/seeds/ProgramsTableSeeder.php
+++ database/seeds/ProgramsTableSeeder.php
@@ -1,24 +1,32 @@
 <?php
 
+use Database\Traits\JsonSeederTrait;
 use Illuminate\Database\Seeder;
 
 class ProgramsTableSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    // precon: service provider seeder
-    public function run()
-    {
-      $serviceProviders = \App\ServiceProvider::all();
-      foreach ($serviceProviders as $serviceProvder)
-      {
-        $serviceProvder->programs()->create([
-          'name' => $serviceProvder->name,
-          'description'=>$serviceProvder->description
-        ]);
+  /**
+   * Run the database seeds.
+   *
+   * @return void
+   */
+  // precon: service provider seeder
+  use JsonSeederTrait;
+
+  public function run()
+  {
+    $dataUrl = base_path() . '/database/data/whoHasWhatPrograms.json';
+    $convertedDataSet = $this->getDataFromJson($dataUrl);
+    foreach ($convertedDataSet as $serviceProviderName => $programTemplateNames) {
+      // find the service provider
+      $serviceProvider = \App\ServiceProvider::findByName($serviceProviderName);
+      foreach ($programTemplateNames as $programTemplateName) {
+        $programTemplateId = \App\Programs\ProgramTemplate::findByName($programTemplateName)->id;
+        $serviceProvider->programs()->create(array(
+            "program_template_id" => $programTemplateId
+          )
+        );
       }
     }
+  }
 }
