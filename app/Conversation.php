29e28c83e92dd80874579ddeac8f820316550068
dev-seahouse <xinkenan.11@hotmail.com>
2017-08-01 17:25:19 +0800

    add program id fillable

--- app/Conversation.php
+++ app/Conversation.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Conversation extends Model
 {
-  protected $fillable = ['message', 'user_id', 'group_id'];
+  protected $fillable = ['message', 'user_id', 'program_id'];
   public function user()
   {
     return $this->belongsTo(User::class);
