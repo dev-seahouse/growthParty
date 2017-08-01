bec5605c6a6045545ee9c8781680ca6a073665fd
dev-seahouse <xinkenan.11@hotmail.com>
2017-08-01 14:04:12 +0800

    add conversation model

--- app/Conversation.php
+++ app/Conversation.php
@@ -2,9 +2,20 @@
 
 namespace App;
 
+use App\Programs\Program;
 use Illuminate\Database\Eloquent\Model;
 
 class Conversation extends Model
 {
+  protected $fillable = ['message', 'user_id', 'group_id'];
+  public function user()
+  {
+    return $this->belongsTo(User::class);
+  }
+
+  public function program()
+  {
+    return $this->belongsTo(Program::class);
+  }
     //
 }
