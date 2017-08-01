32d86888a29d8ecc7c608bb8de42e196a4a60729
dev-seahouse <xinkenan.11@hotmail.com>
2017-08-01 14:04:34 +0800

    add conversation controller

--- app/Http/Controllers/ConversationController.php
+++ app/Http/Controllers/ConversationController.php
@@ -2,9 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Conversation;
+use App\Events\NewMessage;
 use Illuminate\Http\Request;
 
 class ConversationController extends Controller
 {
-    //
+  public function store()
+  {
+    $conversation = Conversation::create([
+      'message'    => request('message'),
+      'program_id' => request('program_id'),
+      'user_id'    => auth()->user()->id
+    ]);
+
+    $conversation->load('user');
+    broadcast(new NewMessage($conversation))->toOthers();
+    return $conversation->load('user');
+  }
 }
