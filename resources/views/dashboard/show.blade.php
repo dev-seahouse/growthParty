3bb8f18b74179b21e011120691e7c4b7f2dc839d
dev-seahouse <xinkenan.11@hotmail.com>
2017-08-01 16:08:21 +0800

    add program id property

--- resources/views/dashboard/show.blade.php
+++ resources/views/dashboard/show.blade.php
@@ -2,7 +2,7 @@
 @section('content')
   <h1>Hello This is a pogram</h1>
   <h4>Program Name: {{ $program->programName() }}</h4>
-  <group-chat>
+  <group-chat :program_id="{{$program->id}}">
 
   </group-chat>
 @endsection
