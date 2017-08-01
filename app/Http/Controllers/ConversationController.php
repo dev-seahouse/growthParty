3e27f713b5d83c9eed1a09b3dc68555a2571281a
dev-seahouse <xinkenan.11@hotmail.com>
2017-08-01 23:36:40 +0800

    add fetching history messages

--- app/Http/Controllers/ConversationController.php
+++ app/Http/Controllers/ConversationController.php
@@ -8,16 +8,21 @@ use Illuminate\Http\Request;
 
 class ConversationController extends Controller
 {
+
+  public function show($id)
+  {
+    $chatHistory = Conversation::where('program_id', $id);
+    return $chatHistory->with('user')->get();
+  }
+
   public function store()
   {
     $conversation = Conversation::create(array(
       'message'    => request('message'),
       'program_id' => request('program_id'),
-      'program_id' => request('program_id'),
       'user_id'    => auth()->user()->id
     ));
 
-    $conversation->load('user');
     broadcast(new NewMessage($conversation))->toOthers();
     return $conversation->load('user');
   }
