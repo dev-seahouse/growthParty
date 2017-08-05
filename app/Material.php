5eb6d17b0650ffc14ff31ce04e01a7e4be5394a6
dev-seahouse <xinkenan.11@hotmail.com>
2017-08-05 17:43:07 +0800

    add php doc for model

--- app/Material.php
+++ app/Material.php
@@ -9,6 +9,20 @@ use Illuminate\Database\Eloquent\Model;
  *
  * @property-read \App\Programs\Program $program
  * @mixin \Eloquent
+ * @property int $id
+ * @property int $program_id
+ * @property string $name
+ * @property string $type
+ * @property string $directory
+ * @property \Carbon\Carbon|null $created_at
+ * @property \Carbon\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Material whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Material whereDirectory($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Material whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Material whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Material whereProgramId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Material whereType($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Material whereUpdatedAt($value)
  */
 class Material extends Model
 {
