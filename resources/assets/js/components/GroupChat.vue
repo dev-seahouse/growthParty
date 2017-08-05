a88c71f31687292e132c76362fb439cc24088d7d
dev-seahouse <xinkenan.11@hotmail.com>
2017-08-05 21:15:17 +0800

    add show user typing feature

--- resources/assets/js/components/GroupChat.vue
+++ resources/assets/js/components/GroupChat.vue
@@ -2,6 +2,9 @@
   <div class="chat restrained bottom right">
     <div class="header" @click="toggleChat">
       <span class="title">Group Chat</span>
+      <transition name="fade">
+        <span v-show="typing" class="subtitle"><i class="fa fa-bullhorn icon" aria-hidden="true"></i>Someone is typing ...</span>
+      </transition>
       <a href="#" class="minimize"></a>
     </div>
     <div class="body">
@@ -12,6 +15,7 @@
           <div class="info">
             <span>
               {{ conversation.user.name }}
+
             </span>
 
             <span class="date">
@@ -26,11 +30,12 @@
 
         </li>
       </ul>
-      <div class="whostyping">
+      <div class="whostyping" v-show="typing">
+        @{{ this.typing_user }} is typing
       </div>
       <div class="input-group controls">
         <input id="btn-input" class="input" type="text" placeholder="type your message here..." v-model="message"
-               @keyup.enter="store()" autofocus>
+               @keyup.enter="store()" @keydown="isTyping" @keyup="notTyping" autofocus>
         <button id="input-group-button btn-chat" class="button" @click.prevent="store">Send</button>
       </div>
     </div>
@@ -47,15 +52,43 @@
       return {
         conversations: [],
         message: '',
+        typing: false,
+        typing_user: ''
       }
+    },
+    created () {
+      let _this = this
+      Echo.private('programs.' + this.program_id)
+        .listenForWhisper('typing', (e) => {
+          this.typing_user = e.user
+          this.typing = e.typing
+
+          setTimeout(function () {
+            _this.typing = false
+          }, 1500)
+        })
     },
     mounted () {
       this.fetchChatHistory()
       this.listenForNewMessage()
     },
+
     methods: {
       toggleChat () {
         $('.body').slideToggle(300, 'swing')
+      },
+      isTyping () {
+        let _this = this
+        let channel = Echo.private('programs.' + this.program_id)
+        setTimeout(function () {
+          channel.whisper('typing', {
+            user: _this.curr_user.name,
+            typing: true
+          })
+        }, 300)
+      },
+      notTyping () {
+        this.typing = false
       },
       fetchChatHistory () {
         axios.get('/conversations/' + this.program_id)
