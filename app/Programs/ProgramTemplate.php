f65074fccca81e0f3a9efb8a73a2dc5f703cd9e2
dev-seahouse <xinkenan.11@hotmail.com>
2017-08-05 17:43:32 +0800

    add php doc for model

--- app/Programs/ProgramTemplate.php
+++ app/Programs/ProgramTemplate.php
@@ -4,6 +4,24 @@ namespace App\Programs;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Programs\ProgramTemplate
+ *
+ * @property int $id
+ * @property string $name
+ * @property string|null $description
+ * @property array $matching_requirements
+ * @property \Carbon\Carbon|null $created_at
+ * @property \Carbon\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Programs\Program[] $programs
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Programs\ProgramTemplate whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Programs\ProgramTemplate whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Programs\ProgramTemplate whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Programs\ProgramTemplate whereMatchingRequirements($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Programs\ProgramTemplate whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Programs\ProgramTemplate whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class ProgramTemplate extends Model
 {
   protected $casts = [
