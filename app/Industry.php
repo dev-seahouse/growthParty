0e824559e625b574b9b09c71bf40461117fd0785
dev-seahouse <xinkenan.11@hotmail.com>
2017-08-05 17:43:00 +0800

    add phpdoc for model

--- app/Industry.php
+++ app/Industry.php
@@ -4,6 +4,19 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Industry
+ *
+ * @property int $id
+ * @property string $name
+ * @property \Carbon\Carbon|null $created_at
+ * @property \Carbon\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Industry whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Industry whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Industry whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Industry whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Industry extends Model
 {
 
