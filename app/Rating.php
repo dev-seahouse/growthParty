c544a674d9b1393847c2ddabe782b5c575a786ec
dev-seahouse <xinkenan.11@hotmail.com>
2017-08-05 17:43:39 +0800

    add php doc for model

--- app/Rating.php
+++ app/Rating.php
@@ -4,6 +4,24 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Rating
+ *
+ * @property int $id
+ * @property int $service
+ * @property int $communication
+ * @property int $service_provider_id
+ * @property \Carbon\Carbon|null $created_at
+ * @property \Carbon\Carbon|null $updated_at
+ * @property-read \App\ServiceProvider $serviceProvider
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Rating whereCommunication($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Rating whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Rating whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Rating whereService($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Rating whereServiceProviderId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Rating whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Rating extends Model
 {
   public function serviceProvider()
