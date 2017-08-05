9ad4aaeeb496e050f1bb93480141fcca0fb0f506
dev-seahouse <xinkenan.11@hotmail.com>
2017-08-05 17:45:51 +0800

    parse user property to js var

--- resources/views/dashboard/show.blade.php
+++ resources/views/dashboard/show.blade.php
@@ -1,8 +1,13 @@
 @extends('layouts.master')
 @section('content')
-  <h1>Hello This is a pogram</h1>
-  <h4>Program Name: {{ $program->programName() }}</h4>
-  <group-chat :program_id="{{$program->id}}">
-
-  </group-chat>
+    <h1>Hello This is a pogram</h1>
+    <h4>Program Name: {{ $program->programName() }}</h4>
+    <group-chat :program_id="{{$program->id}}" :curr_user="{{ Auth::user() }}"></group-chat>
 @endsection
+@push('page_styles')
+    <style>
+        footer {
+          display:none;
+        }
+    </style>
+@endpush
