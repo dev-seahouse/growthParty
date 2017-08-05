ec192c0d251e3dc3e71bbc6349fb3c5697e30507
dev-seahouse <xinkenan.11@hotmail.com>
2017-08-05 17:42:32 +0800

    add show message datetime

--- app/Conversation.php
+++ app/Conversation.php
@@ -3,8 +3,28 @@
 namespace App;
 
 use App\Programs\Program;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Conversation
+ *
+ * @property int $id
+ * @property string|null $message
+ * @property int $user_id
+ * @property int $program_id
+ * @property \Carbon\Carbon|null $created_at
+ * @property \Carbon\Carbon|null $updated_at
+ * @property-read \App\Programs\Program $program
+ * @property-read \App\User $user
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Conversation whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Conversation whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Conversation whereMessage($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Conversation whereProgramId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Conversation whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Conversation whereUserId($value)
+ * @mixin \Eloquent
+ */
 class Conversation extends Model
 {
   protected $fillable = ['message', 'user_id', 'program_id'];
@@ -17,5 +37,10 @@ class Conversation extends Model
   {
     return $this->belongsTo(Program::class);
   }
-    //
+
+  public function getUpdatedAtAttribute($value)
+  {
+    $time = Carbon::parse($value);
+    return $time->toDayDateTimeString();
+  }
 }
