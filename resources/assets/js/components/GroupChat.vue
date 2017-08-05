5e5dc069905d14967ddac8f92ea1d23421cb0bfa
dev-seahouse <xinkenan.11@hotmail.com>
2017-08-05 17:44:32 +0800

    add chat toggler and animation

--- resources/assets/js/components/GroupChat.vue
+++ resources/assets/js/components/GroupChat.vue
@@ -1,57 +1,82 @@
 <template>
-    <div class="callout">
-      <h5>This is a group chat</h5>
-      <ul class="chat">
-        <li v-for="conversation in conversations">
-          <div class="chat-body">
-            {{ conversation.user.name }} says:
+  <div class="chat restrained bottom right">
+    <div class="header" @click="toggleChat">
+      <span class="title">Group Chat</span>
+      <a href="#" class="minimize"></a>
+    </div>
+    <div class="body">
+      <ul class="thread no-bullet">
+
+        <li class="message" v-for="conversation in conversations">
+
+          <div class="info">
+            <span>
+              {{ conversation.user.name }}
+            </span>
+
+            <span class="date">
+              {{ conversation.updated_at }}
+            </span>
+
+          </div>
+
+          <div class="content">
             {{ conversation.message }}
           </div>
+
         </li>
       </ul>
-
-      <div class="footer">
-        <input id="btn-input" type="text" placeholder="type your message here..." v-model="message" @keyup.enter="store()" autofocus>
-        <button id="btn-chat" class="button" @click.prevent="store">Send</button>
+      <div class="whostyping">
       </div>
-
+      <div class="input-group controls">
+        <input id="btn-input" class="input" type="text" placeholder="type your message here..." v-model="message"
+               @keyup.enter="store()" autofocus>
+        <button id="input-group-button btn-chat" class="button" @click.prevent="store">Send</button>
+      </div>
     </div>
+
+  </div>
 </template>
 <script>
   export default {
-    props: ['program_id'],
-    data() {
+    props: [
+      'program_id',
+      'curr_user',
+    ],
+    data () {
       return {
         conversations: [],
-        message:'',
-      };
+        message: '',
+      }
     },
-    mounted() {
-      this.fetchChatHistory();
-      this.listenForNewMessage();
+    mounted () {
+      this.fetchChatHistory()
+      this.listenForNewMessage()
     },
     methods: {
-      fetchChatHistory() {
-        axios.get('/conversations/'+this.program_id)
-          .then((response) => {
-          this.conversations = response.data;
-            //this.conversations.push(response.data);
-          });
+      toggleChat () {
+        $('.body').slideToggle(300, 'swing')
       },
-      store() {
+      fetchChatHistory () {
+        axios.get('/conversations/' + this.program_id)
+          .then((response) => {
+            this.conversations = response.data
+          })
+      },
+      store () {
         axios.post('/conversations', {message: this.message, program_id: this.program_id})
           .then((response) => {
-            this.message = '';
-            this.conversations.push(response.data);
-          });
+            this.message = ''
+            this.conversations.push(response.data)
+          })
       },
 
-      listenForNewMessage() {
+      listenForNewMessage () {
         Echo.private('programs.' + this.program_id)
           .listen('NewMessage', (e) => {
             // console.log(e);
-            this.conversations.push(e);
-          });
+            this.conversations.push(e)
+          })
       }
     }
   }
