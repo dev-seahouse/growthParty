9a935a2843ef9b1abc18df97947fbb4897ac051b
dev-seahouse <xinkenan.11@hotmail.com>
2017-08-07 05:23:19 +0800

    reformat

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -1,26 +1,26 @@
 // window._ = require('lodash');
-
 /**
  * We'll load jQuery and the Bootstrap jQuery plugin which provides support
  * for JavaScript based Bootstrap features such as modals and tabs. This
  * code may be modified to fit the specific needs of your application.
  */
-import Echo from 'laravel-echo';
+import Echo from 'laravel-echo'
+
 try {
-    global.$ = global.jQuery = window.$ = window.jQuery = require('jquery')
-    require('what-input')
-    window.Headroom = require('headroom.js')
+  global.$ = global.jQuery = window.$ = window.jQuery = require('jquery')
+  require('what-input')
+  window.Headroom = require('headroom.js')
 } catch (e) {
-    console.log(e)
+  console.log(e)
 }
 
 require('jquery.counterup')
 require('select2')
-    /**
-     * We'll load the axios HTTP library which allows us to easily issue requests
-     * to our Laravel back-end. This library automatically handles sending the
-     * CSRF token as a header based on the value of the "XSRF" token cookie.
-     */
+/**
+ * We'll load the axios HTTP library which allows us to easily issue requests
+ * to our Laravel back-end. This library automatically handles sending the
+ * CSRF token as a header based on the value of the "XSRF" token cookie.
+ */
 
 window.axios = require('axios')
 window.axios.defaults.headers.common['X-CSRF-TOKEN'] = window.Laravel.csrfToken
@@ -32,10 +32,10 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
  * allows your team to easily build robust real-time web applications.
  */
 
-window.Pusher = require('pusher-js');
+window.Pusher = require('pusher-js')
 window.Echo = new Echo({
-    broadcaster: 'pusher',
-    key: '0f79f672938bb64dc222',
-    cluster:'ap1',
-    encrypted: true
+  broadcaster: 'pusher',
+  key: '0f79f672938bb64dc222',
+  cluster: 'ap1',
+  encrypted: true
 })
