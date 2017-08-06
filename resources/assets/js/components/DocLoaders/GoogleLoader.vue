3b0927511b20010fe52e3e5caef2670c5c61efba
dev-seahouse <xinkenan.11@hotmail.com>
2017-08-07 03:53:31 +0800

    add office loader for office files

---
+++ resources/assets/js/components/DocLoaders/GoogleLoader.vue
@@ -0,0 +1,10 @@
+<template>
+  <iframe :src="'//docs.google.com/viewer?url=' + uri + '&embedded=true'" title="Embedded Document" style="width:100%; height:600px; border: none;"></iframe>
+</template>
+<script>
+  export default {
+    props: [
+      'uri'
+    ]
+  }
+</script>
