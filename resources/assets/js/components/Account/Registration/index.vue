0324ea21350f264b89e200c476aba36d2879aae8
dev-seahouse <xinkenan.11@hotmail.com>
2017-08-06 18:16:26 +0800

    format index

--- resources/assets/js/components/Account/Registration/index.vue
+++ resources/assets/js/components/Account/Registration/index.vue
@@ -2,75 +2,73 @@
   <form action="/register" method="post">
     <h3>Register</h3>
     <component
-      :is="stepView"
-      :detail="detail"
-      @prev="executePrev"
-      @next="executeNext">
+        :is="stepView"
+        :detail="detail"
+        @prev="executePrev"
+        @next="executeNext">
     </component>
   </form>
 </template>
 
 <script>
-import StepOne from './Step1';
-import StepTwo from './Step2';
+  import StepOne from './Step1'
+  import StepTwo from './Step2'
 
-const components = {
-  'step-1': StepOne,
-  'step-2': StepTwo,
-};
-const componentsCount = Object.keys(components).length;
-let formParsley;
+  const components = {
+    'step-1': StepOne,
+    'step-2': StepTwo,
+  }
+  const componentsCount = Object.keys(components).length
+  let formParsley
 
-export default {
-  components,
-  computed: {
-    stepView() {
-      return `step-${this.currentStep}`;
+  export default {
+    components,
+    computed: {
+      stepView () {
+        return `step-${this.currentStep}`
+      },
     },
-  },
-  data() {
-    return {
-      currentStep: 1,
-      detail: {},
-    };
-  },
-  methods: {
-    executePrev() {
-      if (this.currentStep > 1) {
-        this.currentStep--;
+    data () {
+      return {
+        currentStep: 1,
+        detail: {},
       }
     },
-    executeNext(response) {
-      formParsley.whenValidate()
-        .then(() => {
-          const result = (typeof response === 'function') ? response() : response;
-          if (result) {
-            // Merge result
-            Object.keys(result).forEach((key) => {
-              this.detail[key] = result[key];
-            });
+    methods: {
+      executePrev () {
+        if (this.currentStep > 1) {
+          this.currentStep--
+        }
+      },
+      executeNext (response) {
+        formParsley.whenValidate()
+          .then(() => {
+            const result = (typeof response === 'function') ? response() : response
+            if (result) {
+              // Merge result
+              Object.keys(result).forEach((key) => {
+                this.detail[key] = result[key]
+              })
 
-            if (this.currentStep < componentsCount) {
-              // Move to next section
-              this.currentStep++;
-              formParsley = $(this.$el).parsley();
-
-
-            } else {
-              axios.post('/register', this.detail)
-                .then(function(res){
-                  console.log(res);
-                  // if registration sucessful please redirect to /dashboard
-                  // if not sucessful, please show error message, demo is inside login.vue  + login controller. there is also a regex to validate singapore number e.g starting with +98 not +12
-                })
-                .catch(console.log);
+              if (this.currentStep < componentsCount) {
+                // Move to next section
+                this.currentStep++
+                formParsley = $(this.$el).parsley()
+              } else {
+                axios.post('/register', this.detail)
+                  .then(function (res) {
+                    console.log(res)
+                    // if registration sucessful please redirect to /dashboard
+                    // if not sucessful, please show error message, demo is inside login.vue  + login controller. there is also a regex to validate singapore number e.g starting with +98 not +12
+                  })
+                  .catch(console.log)
+              }
             }
-          }
-        });
+          })
+      },
     },
-  },
-  mounted() {
-    formParsley = $(this.$el).parsley();
+    mounted () {
+      formParsley = $(this.$el).parsley()
+    }
   }
-};
 </script>
