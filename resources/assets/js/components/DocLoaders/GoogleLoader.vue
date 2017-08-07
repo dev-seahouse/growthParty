15008e2fa206f20acbe9a9972644a16827440f1c
dev-seahouse <xinkenan.11@hotmail.com>
2017-08-07 20:50:25 +0800

    reduce google doc loader size for mobile

--- resources/assets/js/components/DocLoaders/GoogleLoader.vue
+++ resources/assets/js/components/DocLoaders/GoogleLoader.vue
@@ -1,5 +1,8 @@
 <template>
-  <iframe :src="'//docs.google.com/viewer?url=' + uri + '&embedded=true'" title="Embedded Document" style="width:100%; height:600px; border: none;"></iframe>
+  <div class="iframe-holder">
+    <iframe :src="'//docs.google.com/viewer?url=' + uri + '&embedded=true&hl=en'" title="Embedded Document" style="width:100%; height:550px; border: none;"></iframe>
+    <a :href="uri" class="download-button" download><i class="fa fa-cloud-download" aria-hidden="true"></i><span class="link-text">Download</span></a>
+  </div>
 </template>
 <script>
   export default {
