e90c3aa0ac4bf005da7cbc922dc2bbd7aedfbb26
dev-seahouse <xinkenan.11@hotmail.com>
2017-08-07 20:50:46 +0800

    reduce office doc loader size and add progress loading animation

--- resources/assets/js/components/DocLoaders/OfficeLoader.vue
+++ resources/assets/js/components/DocLoaders/OfficeLoader.vue
@@ -1,9 +1,13 @@
 <template>
-  <iframe
-      :src="'//view.officeapps.live.com/op/embed.aspx?src=' +  uri"
-      width='100%' height='600px' frameborder='0'>This is an embedded <a target='_blank' href='https://office.com'>Microsoft Office</a>
-    presentation, powered by <a target='_blank' href='https://office.com/webapps'>Office Online</a>.
-  </iframe>
+  <div>
+    <iframe
+        :src="'//view.officeapps.live.com/op/embed.aspx?src=' +  uri"
+        width='100%' height='550px' frameborder='0'>This is an embedded <a target='_blank' href='https://office.com'>Microsoft Office</a>
+      presentation, powered by <a target='_blank' href='https://office.com/webapps'>Office Online</a>.
+    </iframe>
+    <a :href="uri" class="download-button" download><i class="fa fa-cloud-download" aria-hidden="true"></i><span class="link-text">Download</span></a>
+  </div>
+
 </template>
 <script>
 export default {
